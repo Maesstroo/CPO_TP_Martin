5ec270b4638d1103f1c7921dd8c782656f57666d
guilhem <guilhem@10.3.60.104>
2023-09-18 12:54:25 +0200

    creation du projet

---
+++ PremierProjetSurGithub/src/premierprojetsurgithub/PremierProjetSurGithub.java
@@ -0,0 +1,21 @@
+package premierprojetsurgithub;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author guilhem
+ */
+public class PremierProjetSurGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
