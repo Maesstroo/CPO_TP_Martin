e20041ae5ad4bdaa5f9d59c350257c0c5812f496
guilhem <guilhem@10.3.60.104>
2023-10-04 00:28:05 +0200

    TP1 exo 2 Martin Guilhem TDB

--- TP1_convertisseur_Martin_Guilhem/src/tp1_convertisseur_martin_guilhem/TP1_convertisseur_Martin_Guilhem.java
+++ TP1_convertisseur_Martin_Guilhem/src/tp1_convertisseur_martin_guilhem/TP1_convertisseur_Martin_Guilhem.java
@@ -19,22 +19,37 @@ public class TP1_convertisseur_Martin_Guilhem {
         double vr;
         double commande; 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Entrez la température");
+        System.out.println("Entrez la température dans l'unité que vous désirez:");
         vr=sc.nextDouble();
         System.out.println("Choisir votre convertion:\n 1 pour des Kelvin en Celcius\n2 pour des Farenheit en Celcius\n 3 pour des Celcius en Farenheit\n4 pour des Kelvin en Farenheit\n5 pour des Farenheit en Kelvin\n6 pour des Celcius en Kelvin");
         commande=sc.nextDouble();
         
-        double CversK = CelciusVersKelvin(vr);
-        System.out.println("Les "+vr+" degrès celcius donnent "+CversK+"Kelvin");
-         
-        double KversC= KelvinVersCelcius(vr);
-        System.out.println("Les "+vr+" degrès celcius donnent "+KversC +"Kelvin");
         
-        double CversF = CelciusVersFarenheit(vr);
-        System.out.println("Les "+vr+" degrès celcius donnent "+CversF+"Kelvin");
-        
+        if (commande==1){
+            double KversC= KelvinVersCelcius(vr);
+            System.out.println("Les "+vr+" degrès Kelvin donnent "+KversC +"degrésCelcius");}
+    
+        if(commande==2){
         double FversC = FarenheitVersCelcius(vr);
-        System.out.println("Les "+vr+" degrès celcius donnent "+FversC+"Kelvin");
+        System.out.println("Les "+vr+" degrès Farenheit donnent "+FversC+"degrés Celcius");}
+        
+        if ( commande==3) {
+        double CversF = CelciusVersFarenheit(vr);
+        System.out.println("Les "+vr+" degrès Celcius donnent "+CversF+"Farenheit");}
+        
+        
+        if(commande==4){
+        double KversF= KelvinVersFarenheit(vr); 
+        System.out.println("Les "+vr+" Kelvin donnent "+KversF+"Farenheit"); }
+        
+        if (commande==5) {
+        double FversK = FarenheitVersKelvin (vr);
+        System.out.println("Les "+vr+" degrès Farenheit donnent "+FversK+"Kelvin"); }
+        
+        if (commande==6) {
+        double CversK = CelciusVersKelvin(vr);
+        System.out.println("Les "+vr+" degrès Celcius donnent "+CversK+"Kelvin");}
+        
          
     
     }
@@ -59,7 +74,22 @@ public class TP1_convertisseur_Martin_Guilhem {
      Celcius2 = tFaren / (9/5) - 32 ; 
     return Celcius2 ;
     }
-
+    
+    public static double KelvinVersFarenheit (double tKelvin2) { 
+    double Celcius ;
+    double Faren ;
+    Celcius = tKelvin2 - 273.15; 
+    Faren = Celcius * (9/5) + 32 ; 
+    return Faren;}
+  
+    public static double FarenheitVersKelvin (double tFaren2) { 
+    double Kelvin ;
+    double Celcius ;
+    Celcius = tFaren2 - 273.15; 
+    Kelvin = Celcius * (9/5) + 32 ; 
+    return Kelvin;}
+    
+ 
 
 
 }
